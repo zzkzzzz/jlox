05ed4c88e4b1af90cf718c24ab9522410f40f8ff
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-10-24 17:55:29 +0800

    add control flow

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -17,9 +17,11 @@ public class GenerateAst {
         defineAst(outputDir, "Expr",
                 Arrays.asList("Assign   : Token name, Expr value", "Binary   : Expr left, Token operator, Expr right",
                         "Grouping : Expr expression", "Literal  : Object value",
-                        "Unary    : Token operator, Expr right", "Variable : Token name"));
+                        "Logical  : Expr left, Token operator, Expr right", "Unary    : Token operator, Expr right",
+                        "Variable : Token name"));
         defineAst(outputDir, "Stmt", Arrays.asList("Block      : List<Stmt> statements", "Expression : Expr expression",
-                "Print      : Expr expression", "Var        : Token name, Expr initializer"));
+                "If         : Expr condition, Stmt thenBranch," + " Stmt elseBranch", "Print      : Expr expression",
+                "Var        : Token name, Expr initializer", "While      : Expr condition, Stmt body"));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
